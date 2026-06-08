cefca155dbf1318360278bca4f2d2c69370e3c25
Dog863 <davi.dognani.santos@uel.br>
2026-06-08 13:16:45 -0300

    Refactor component processing and command handling

--- src/leitor_qry.c
+++ src/leitor_qry.c
@@ -7,7 +7,13 @@
 
 #define MAX_LINE 1024
 
-// Estrutura auxiliar para componentes conexos
+// Estrutura para componentes conexos
+typedef struct {
+    int *visitado;
+    int n_vertices;
+    Grafo *g;
+} DadosDFS;
+
 typedef struct contexto_qry {
     ListaQuadras *quadras;
     Grafo *grafo;
@@ -18,25 +24,19 @@ typedef struct contexto_qry {
     double altura_svg;
 } ContextoQRY;
 
-typedef struct componentes_conexos {
-    int *visitado;
-    int n_vertices;
-    Grafo *g;
-    int num_componentes;
-} ComponentesConexos;
 
-static void dfs(ComponentesConexos *comp, int v, int *comp_id, int id) {
-    comp->visitado[v] = 1;
+static void dfs_componente(DadosDFS *dados, int v, int *comp_id, int id, double vl) {
+    dados->visitado[v] = 1;
     comp_id[v] = id;
     
-    Vertice *vert = grafo_get_vertice(comp->g, v);
-    Aresta *a = vertice_get_primeira_aresta(vert, comp->g);
+    Vertice *vert = grafo_get_vertice(dados->g, v);
+    Aresta *a = vertice_get_primeira_aresta(vert, dados->g);
     while (a) {
-        if (aresta_get_velocidade(a) < 5.0) {  // velocidade baixa
+        if (aresta_get_velocidade(a) < vl) {
             Vertice *dest = aresta_get_destino(a);
             int w = vertice_get_indice(dest);
-            if (!comp->visitado[w]) {
-                dfs(comp, w, comp_id, id);
+            if (!dados->visitado[w]) {
+                dfs_componente(dados, w, comp_id, id, vl);
             }
         }
         a = aresta_get_prox(a);
@@ -45,44 +45,29 @@ static void dfs(ComponentesConexos *comp, int v, int *comp_id, int id) {
 
 static int calcular_componentes_conexos(Grafo *g, double vl, int **comp_id) {
     int n = grafo_num_vertices(g);
-    int *visitado = (int*)calloc(n, sizeof(int));
+    if (n == 0) return 0;
+    
+    DadosDFS dados;
+    dados.visitado = (int*)calloc(n, sizeof(int));
+    dados.n_vertices = n;
+    dados.g = g;
+    
     *comp_id = (int*)calloc(n, sizeof(int));
     int num = 0;
     
     for (int i = 0; i < n; i++) {
-        if (!visitado[i]) {
-            // Verificar se vértice tem alguma aresta com velocidade < vl
-            Vertice *v = grafo_get_vertice(g, i);
-            Aresta *a = vertice_get_primeira_aresta(v, g);
-            int tem_aresta_lenta = 0;
-            while (a) {
-                if (aresta_get_velocidade(a) < vl) {
-                    tem_aresta_lenta = 1;
-                    break;
-                }
-                a = aresta_get_prox(a);
-            }
-            
-            if (tem_aresta_lenta) {
-                // Inicia nova componente conexa
-                num++;
-                // Fazer DFS limitado a arestas lentas
-                // Implementação simplificada
-                visitado[i] = 1;
-                (*comp_id)[i] = num;
-            } else {
-                visitado[i] = 1;
-                (*comp_id)[i] = 0;
-            }
+        if (!dados.visitado[i]) {
+            num++;
+            dfs_componente(&dados, i, *comp_id, num, vl);
         }
     }
     
-    free(visitado);
+    free(dados.visitado);
     return num;
 }
 
 // Estrutura para aresta na AGM
-typedef struct aresta_agm {
+typedef struct {
     int u, v;
     double peso;
     Aresta *aresta;
@@ -116,8 +101,8 @@ static void calcular_agm_velocidade_lenta(Grafo *g, double vl, SVG *svg) {
     int n = grafo_num_vertices(g);
     if (n == 0) return;
     
-    // Coletar arestas com velocidade < vl
-    ArestaAGM *arestas = (ArestaAGM*)malloc(grafo_num_arestas(g) * sizeof(ArestaAGM));
+    int num_arestas = grafo_num_arestas(g);
+    ArestaAGM *arestas = (ArestaAGM*)malloc(num_arestas * sizeof(ArestaAGM));
     int num_arestas_lentas = 0;
     
     for (int i = 0; i < n; i++) {
@@ -141,15 +126,12 @@ static void calcular_agm_velocidade_lenta(Grafo *g, double vl, SVG *svg) {
         return;
     }
     
-    // Ordenar por peso
     qsort(arestas, num_arestas_lentas, sizeof(ArestaAGM), comparar_peso);
     
-    // Kruskal
     int *pai = (int*)malloc(n * sizeof(int));
     int *rank = (int*)calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) pai[i] = i;
     
-    // Marcar arestas selecionadas
     int *selecionada = (int*)calloc(num_arestas_lentas, sizeof(int));
     int num_selecionadas = 0;
     
@@ -163,7 +145,6 @@ static void calcular_agm_velocidade_lenta(Grafo *g, double vl, SVG *svg) {
         }
     }
     
-    // Desenhar arestas selecionadas e aumentar velocidade
     for (int i = 0; i < num_arestas_lentas; i++) {
         if (selecionada[i]) {
             double x1 = vertice_get_x(arestas[i].origem);
@@ -171,10 +152,8 @@ static void calcular_agm_velocidade_lenta(Grafo *g, double vl, SVG *svg) {
             double x2 = vertice_get_x(aresta_get_destino(arestas[i].aresta));
             double y2 = vertice_get_y(aresta_get_destino(arestas[i].aresta));
             
-            // Desenha linha grossa vermelha
             svg_linha(svg, x1, y1, x2, y2, "red", 3.0, NULL);
             
-            // Aumenta velocidade em 50%
             double nova_vel = aresta_get_velocidade(arestas[i].aresta) * 1.5;
             aresta_set_velocidade(arestas[i].aresta, nova_vel);
         }
@@ -186,6 +165,7 @@ static void calcular_agm_velocidade_lenta(Grafo *g, double vl, SVG *svg) {
     free(arestas);
 }
 
+// Processar comando @o?
 static void processar_comando_oreg(ContextoQRY *ctx, char *linha) {
     int reg;
     char cep[20];
@@ -199,25 +179,29 @@ static void processar_comando_oreg(ContextoQRY *ctx, char *linha) {
             quadra_calcular_endereco(q, face, num, &x, &y);
             registro_armazenar(ctx->registros, reg, x, y);
             
-            // Linha vertical pontilhada vermelha
-            svg_linha(ctx->svg, x, 0, x, ctx->altura_svg, "red", 1.0, "5,5");
-            svg_texto(ctx->svg, x + 2, 15, (char[2]){reg + '0', '\0'}, "red", 10);
+            svg_linha(ctx->svg, x, y, x, 0, "red", 1.0, "5,5");
             
-            fprintf(ctx->txt_output, "[*] @o? %d %s %c %d\n", reg, cep, face, num);
+            char r_str[10];
+            snprintf(r_str, sizeof(r_str), "R%d", reg);
+            svg_texto(ctx->svg, x + 2, y - 3, r_str, "red", 10);
+            
+            fprintf(ctx->txt_output, "[*] @o? R%d %s %c %d\n", reg, cep, face, num);
             fprintf(ctx->txt_output, "Coordenada: (%.2f, %.2f)\n", x, y);
         }
     }
 }
 
-static void processar_comando_mv(ContextoQRY *ctx, char *linha) {
+// Processar comando m
+static void processar_comando_m(ContextoQRY *ctx, char *linha) {
     double x, y, w, h, v;
-    if (sscanf(linha, "mv %lf %lf %lf %lf %lf", &x, &y, &w, &h, &v) == 5) {
+    if (sscanf(linha, "m %lf %lf %lf %lf %lf", &x, &y, &w, &h, &v) == 5) {
         grafo_atualizar_velocidade_regiao(ctx->grafo, x, y, w, h, v);
-        fprintf(ctx->txt_output, "[*] mv %.2f %.2f %.2f %.2f %.2f\n", x, y, w, h, v);
+        fprintf(ctx->txt_output, "[*] m %.2f %.2f %.2f %.2f %.2f\n", x, y, w, h, v);
         fprintf(ctx->txt_output, "Velocidades atualizadas na região\n");
     }
 }
 
+// Processar comando regs
 static void processar_comando_regs(ContextoQRY *ctx, char *linha) {
     double vl;
     if (sscanf(linha, "regs %lf", &vl) == 1) {
@@ -227,12 +211,10 @@ static void processar_comando_regs(ContextoQRY *ctx, char *linha) {
         fprintf(ctx->txt_output, "[*] regs %.2f\n", vl);
         fprintf(ctx->txt_output, "Número de componentes conexos: %d\n", n_comps);
         
-        // Cores para os componentes
         const char *cores[] = {"red", "blue", "green", "orange", "purple", 
                                "cyan", "magenta", "yellow", "pink", "brown"};
         
-        // Bounding boxes simplificados
-        for (int c = 1; c <= n_comps; c++) {
+        for (int c = 1; c <= n_comps && c <= 10; c++) {
             double min_x = 1e9, min_y = 1e9, max_x = -1e9, max_y = -1e9;
             int n_verts = grafo_num_vertices(ctx->grafo);
             
@@ -249,10 +231,10 @@ static void processar_comando_regs(ContextoQRY *ctx, char *linha) {
             }
             
             if (min_x < 1e8) {
-                double w = max_x - min_x + 10;
-                double h = max_y - min_y + 10;
-                svg_bounding_box(ctx->svg, min_x - 5, min_y - 5, w, h, 
-                                cores[(c-1) % 10], 0.5);
+                double bw = max_x - min_x + 20;
+                double bh = max_y - min_y + 20;
+                svg_bounding_box(ctx->svg, min_x - 10, min_y - 10, bw, bh, 
+                                cores[(c-1) % 10], 0.3);
             }
         }
         
@@ -260,6 +242,7 @@ static void processar_comando_regs(ContextoQRY *ctx, char *linha) {
     }
 }
 
+// Processar comando exp
 static void processar_comando_exp(ContextoQRY *ctx, char *linha) {
     double vl;
     if (sscanf(linha, "exp %lf", &vl) == 1) {
@@ -269,6 +252,7 @@ static void processar_comando_exp(ContextoQRY *ctx, char *linha) {
     }
 }
 
+// Processar comando p?
 static void processar_comando_p(ContextoQRY *ctx, char *linha) {
     int reg1, reg2;
     char cc[20], cr[20];
@@ -284,7 +268,6 @@ static void processar_comando_p(ContextoQRY *ctx, char *linha) {
             return;
         }
         
-        // Encontrar vértices mais próximos
         Vertice *v_orig = grafo_encontrar_vertice_proximo(ctx->grafo, x1, y1);
         Vertice *v_dest = grafo_encontrar_vertice_proximo(ctx->grafo, x2, y2);
         
@@ -293,9 +276,7 @@ static void processar_comando_p(ContextoQRY *ctx, char *linha) {
             return;
         }
         
-        // Caminho mais curto (distância)
         Caminho *curto = dijkstra_distancia(ctx->grafo, v_orig, v_dest);
-        // Caminho mais rápido (tempo)
         Caminho *rapido = dijkstra_tempo(ctx->grafo, v_orig, v_dest);
         
         if (curto) {
@@ -303,21 +284,7 @@ static void processar_comando_p(ContextoQRY *ctx, char *linha) {
                     caminho_distancia(curto));
             svg_desenhar_caminho(ctx->svg, curto, cc, 3.0);
             
-            // Animação
-            char path_data[1024] = "";
-            for (int i = 0; i < caminho_num_vertices(curto) - 1; i++) {
-                Vertice *v1 = caminho_get_vertice(curto, i);
-                Vertice *v2 = caminho_get_vertice(curto, i + 1);
-                char ponto[128];
-                sprintf(ponto, "M%.2f,%.2f L%.2f,%.2f ", 
-                        vertice_get_x(v1), vertice_get_y(v1),
-                        vertice_get_x(v2), vertice_get_y(v2));
-                strcat(path_data, ponto);
-            }
-            svg_animate_motion(ctx->svg, "circle", 5, 0, 0, "blue", "blue", path_data, 3.0);
-            
-            // Descrição textual do percurso
-            for (int i = 0; i < caminho_num_arestas(curto); i++) {
+            for (int i = 0; i < caminho_num_arestas(curto) && i < 30; i++) {
                 Aresta *a = caminho_get_aresta(curto, i);
                 fprintf(ctx->txt_output, "  - Siga pela %s\n", aresta_get_nome(a));
             }
@@ -330,8 +297,7 @@ static void processar_comando_p(ContextoQRY *ctx, char *linha) {
                     caminho_tempo(rapido));
             svg_desenhar_caminho(ctx->svg, rapido, cr, 3.0);
             
-            // Descrição textual
-            for (int i = 0; i < caminho_num_arestas(rapido); i++) {
+            for (int i = 0; i < caminho_num_arestas(rapido) && i < 30; i++) {
                 Aresta *a = caminho_get_aresta(rapido, i);
                 fprintf(ctx->txt_output, "  - Siga pela %s\n", aresta_get_nome(a));
             }
@@ -339,7 +305,6 @@ static void processar_comando_p(ContextoQRY *ctx, char *linha) {
             fprintf(ctx->txt_output, "Destino inacessível para caminho mais rápido\n");
         }
         
-        // Marcar I e F
         svg_circulo(ctx->svg, vertice_get_x(v_orig), vertice_get_y(v_orig), 8, "green", "black", 2);
         svg_texto(ctx->svg, vertice_get_x(v_orig) - 5, vertice_get_y(v_orig) + 5, "I", "white", 10);
         svg_circulo(ctx->svg, vertice_get_x(v_dest), vertice_get_y(v_dest), 8, "red", "black", 2);
@@ -358,16 +323,17 @@ int executar_arquivo_qry(const char *nome_arquivo, ContextoQRY *ctx, const char 
     }
     
     char linha[MAX_LINE];
+    (void)nome_base_svg;  // Evitar warning
     
-    while (fgets(linha, MAX_LINE, f)) {
+    while (fgets(linha, sizeof(linha), f)) {
         linha[strcspn(linha, "\n")] = 0;
         if (strlen(linha) == 0) continue;
         
-        if (linha[0] == '@' && linha[1] == 'o' && linha[2] == '?') {
+        if (strncmp(linha, "@o?", 3) == 0) {
             processar_comando_oreg(ctx, linha);
         }
-        else if (strncmp(linha, "mv", 2) == 0) {
-            processar_comando_mv(ctx, linha);
+        else if (strncmp(linha, "m ", 2) == 0 || strncmp(linha, "mv", 2) == 0) {
+            processar_comando_m(ctx, linha);
         }
         else if (strncmp(linha, "regs", 4) == 0) {
             processar_comando_regs(ctx, linha);
@@ -375,7 +341,7 @@ int executar_arquivo_qry(const char *nome_arquivo, ContextoQRY *ctx, const char 
         else if (strncmp(linha, "exp", 3) == 0) {
             processar_comando_exp(ctx, linha);
         }
-        else if (linha[0] == 'p' && linha[1] == '?') {
+        else if (strncmp(linha, "p?", 2) == 0) {
             processar_comando_p(ctx, linha);
         }
     }
